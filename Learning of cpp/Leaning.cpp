5b08ba82b8bb0b35a5be06706bdf445341d8cf58
Subash_S <14subaz@gmail.com>
2022-08-12 15:42:13 +0530

    cpp learning started

---
+++ Learning of cpp/Leaning.cpp
@@ -0,0 +1,89 @@
+/*#include<iostream> //input and output
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World...!";
+    return 0;
+}
+
+*/
+// what is namespace?
+/*
+#include<iostream> //input and output
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cout<<"Enter the value of A, B: ";
+    cin>>a>>b;
+    cout<<"Value of A: "<<a+b;
+    return 0;
+
+
+}
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a;
+    cout<<"Enter the para: ";
+    getline(cin, a);
+    cout<<a;
+
+    return 0;
+}
+*/
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+
+    //string a="Welcome to Subash Home";
+    /*
+    string a("Welcome to Subash Home");
+    cout<<a<<endl;
+    return 0;
+    *//*
+    //String concatenation
+
+    string firstname="Tutor ";
+    string lastname="Joes ";
+
+    cout<<firstname+lastname<<endl;
+    return 0;*/
+
+    //append method
+    /*
+    string firstname="Tutor ";
+    string lastname="Joes ";
+    string fullname=firstname.append(lastname);
+    cout<<fullname<<endl;
+    return 0;
+    *//*
+    //String access
+    string firstname="Tutor ";
+    string lastname="Joes ";
+    string fullname=firstname.append(lastname);
+    cout<<fullname<<endl;
+    cout<<firstname[0]<<endl;
+    firstname[0]='C';
+    cout<<firstname<<endl;
+
+    return 0;
+    *//*
+    string str ;
+    cout<<"Enter the string: ";
+    cin>>str;
+    cout<<str<<endl;
+    fflush(stdin);
+    cout<<"Enter the string: ";
+    getline(cin, str);
+    cout<<str<<endl;
+    return 0;*/
+}
